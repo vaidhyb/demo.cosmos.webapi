e22529df045c7973af209ec7a01559e65c408788
cloudiqweb <vaidhyanathan@cloudiqtech.com>
2017-08-23 05:54:01 +0000

    Updated the csv and shell script files

--- utility/deploy_settings.ps1
+++ utility/deploy_settings.ps1
@@ -1,2 +1,52 @@
-New-Item c:\vaidhyanathan\source\new_file.txt -type file
-Write-Output "files created successfully $env:computername" >> C:\vaidhyanathan\source\new_file.txt
+Add-Type -AssemblyName System.Web
+
+$currentServerName = $env:computername
+Write-Host $currentServerName
+
+$csvPath = $PSScriptRoot + '\ServerConfig.csv';
+$csvObjects = Import-CSV $csvPath;
+$serverObject = $csvObjects | Where { $_.ServerName -eq $currentServerName}
+If ($serverObject) {
+    $serverName =  $serverObject.ServerName;
+    $hospitalID =  $serverObject.HospitalId;
+    $connectionString =  $serverObject.ConnectionString;
+}
+Else {
+    Write-Host "variable is null" 
+}
+
+$webConfigPath = $PSScriptRoot + '\Web.config'
+$webConfig = (Get-Content $webConfigPath) -as [Xml]
+
+
+$appSettingHospitalID = $webConfig.configuration.appSettings.add | where {$_.Key -eq 'HospitalID'}
+if($appSettingHospitalID) {
+    $appSettingHospitalID.value = $hospitalID;
+}
+else {
+    $newAppSetting = $webConfig.CreateElement("add")
+    $webConfig.configuration.appSettings.AppendChild($newAppSetting)
+    $newAppSetting.SetAttribute("key","HospitalID");
+    $newAppSetting.SetAttribute("value",$hospitalID);
+}
+
+$appSettingServerName = $webConfig.configuration.appSettings.add | where {$_.Key -eq 'ServerName'}
+if($appSettingServerName) {
+    $appSettingServerName.value = $serverName;
+}
+else {
+    $newAppSetting = $webConfig.CreateElement("add")
+    $webConfig.configuration.appSettings.AppendChild($newAppSetting)
+    $newAppSetting.SetAttribute("key","ServerName");
+    $newAppSetting.SetAttribute("value",$serverName);
+}
+
+$root = $webConfig.get_DocumentElement();
+$connectionStringSection = $root.connectionStrings.add.connectionString
+if($connectionStringSection) {
+    $connectionStringSection = $root.connectionStrings.add.connectionString.Replace('{ConnectionString}',$connectionString);
+    $root.connectionStrings.add.connectionString = $connectionStringSection
+}
+
+
+$webConfig.Save($webConfigPath)
